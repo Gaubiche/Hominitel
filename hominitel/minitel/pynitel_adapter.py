93b7f024fed5821c1b44ecdf7a436f2d8ce4f22b
Gautier <thegaubi78@gmail.com>
2025-05-16 21:52:06 +0200

    update adapter

--- hominitel/minitel/pynitel_adapter.py
+++ hominitel/minitel/pynitel_adapter.py
@@ -2,6 +2,9 @@ from hominitel.minitel.adapter import Adapter
 from hominitel.minitel.lib.upynitel import Pynitel
 
 class PynitelAdapter(Pynitel, Adapter):
+    def print(self, text: str):
+        self._print(text)
+
     def get_input(self):
         last_char = self._if()
         if last_char is None or last_char == b'\x1b;cZD':
